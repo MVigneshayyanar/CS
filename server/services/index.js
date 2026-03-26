1b0b292c56e24c9ee26fb64f2bcbab15eed4f773
saravanan1442005 <saravanan14.4.2005@gmail.com>
2026-03-26 14:04:37 +0530

    server removed

--- server/services/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  // Export domain services from here.
-};
--- server/tests/health.test.js
+++
@@ -1,6 +0,0 @@
-const test = require("node:test");
-const assert = require("node:assert/strict");
-
-test("sample health check placeholder", () => {
-  assert.equal(true, true);
-});
