667530e9cf587895d9bb998f501752e606691b53
MVigneshayyanar <vicky69tn@gmail.com>
2026-03-27 10:16:48 +0530

    chore: remove a file

--- server/utils/logger.js
+++
@@ -1,9 +0,0 @@
-const log = (...args) => {
-  // Central logger wrapper for future structured logging.
-  // eslint-disable-next-line no-console
-  console.log(...args);
-};
-
-module.exports = {
-  log,
-};
